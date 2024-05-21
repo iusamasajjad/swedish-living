242ca6c585bef9a1645940c91fee66680b43cfcf
iusamasajjad <i.usamasajjad@gmail.com>
2024-05-22 03:47:34 +0500

    lift up

---
+++ assets/script/script.js
@@ -0,0 +1,27 @@
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+// ============ mobile-hamburger-function
+// menu-function
+
+function menuToggle() {
+    var x = document.getElementById("mobile-menu");
+    x.style.width = "100%";
+  }
+  function menuClose() {
+    var x = document.getElementById("mobile-menu");
+    x.style.width = "0%";
+  }
